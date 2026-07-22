d814c7c3365ef963bee3e8a60cbbf757fd40b271
cfh2dc97gz-sudo <cfh2dc97gz@privaterelay.appleid.com>
2026-07-22 21:46:02 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2424,3 +2424,18 @@ if(numberCallback){
 
 }
 };
+let lastTouchEnd = 0;
+
+document.addEventListener("touchend", function(e){
+
+    const now = Date.now();
+
+    if(now - lastTouchEnd <= 300){
+
+        e.preventDefault();
+
+    }
+
+    lastTouchEnd = now;
+
+}, { passive:false });
