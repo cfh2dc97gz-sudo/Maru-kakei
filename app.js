11a27e1b07d546db056798f4725b216e004263f3
cfh2dc97gz-sudo <cfh2dc97gz@privaterelay.appleid.com>
2026-07-25 19:47:22 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1792,6 +1792,54 @@ function getFiscalIncomeHistory(){
     );
 
 }
+function drawIncomeHistory(){
+
+    const area =
+        document.getElementById("incomeHistoryList");
+
+    if(!area) return;
+
+    const list =
+        getFiscalIncomeHistory();
+
+    area.innerHTML = "";
+
+    if(list.length === 0){
+
+        area.innerHTML =
+            "<p>まだ収入履歴はありません😊</p>";
+
+        return;
+
+    }
+
+    list.forEach(item=>{
+
+        area.innerHTML += `
+
+<button class="setting-item">
+
+    <span>
+
+        ${item.type}<br>
+
+        <small>${item.date}</small>
+
+    </span>
+
+    <span>
+
+        ¥${item.amount.toLocaleString()}
+
+    </span>
+
+</button>
+
+`;
+
+    });
+
+}
 function getMonthlySpent(year,month){
 
 const data =
