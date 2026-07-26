f293f3e6ea4149a56cee7f7262d5c32582f95e92
cfh2dc97gz-sudo <cfh2dc97gz@privaterelay.appleid.com>
2026-07-26 21:38:40 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1067,6 +1067,17 @@ case "income":
 
     break;
 
+            case "categoryHistory":
+
+    categoryHistoryPage.style.display = "block";
+
+    navButtons[3].classList.add("active");
+
+    drawCategoryHistory();
+
+    lastPage = "setting";
+
+    break;
     }
 
     if(page !== "category"){
