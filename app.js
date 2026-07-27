c3c9de7aa33d713bd64721de0717b1ee5a1160ce
cfh2dc97gz-sudo <cfh2dc97gz@privaterelay.appleid.com>
2026-07-27 11:40:45 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2206,7 +2206,7 @@ function openAnnualCategory(index){
     showPage("category");
     
     // document.getElementById("editAnnualCategory").style....
-    document.getElementById("addAnnualHistory").style.display="block";
+   // document.getElementById("addAnnualHistory").style.display="block";
 
     document.getElementById("categoryTitle").textContent=
         category.title;
